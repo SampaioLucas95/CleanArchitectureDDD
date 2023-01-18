974d9be52a9164737d308c214d7ed5b34687a398
Lucas Souza <lucas.sampaio@bs2.com>
2023-01-18 17:57:51 -0300

    add redis service, with pendences

---
+++ SolutionName/SolutionName.Service/Redis/ICacheService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionName.Service.Redis
+{
+    public interface ICacheService
+    {
+        T Get<T>(string key);
+
+        void Set<T>(string key, T content);
+
+        void Remove(string key);
+
+    }
+}
