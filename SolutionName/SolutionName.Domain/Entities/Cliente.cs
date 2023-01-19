7bd477b72809ee3a2f8ee63fea1333b622c961ce
Lucas Souza <lucas.sampaio@bs2.com>
2023-01-18 23:10:16 -0300

    add Test domain

--- SolutionName/SolutionName.Domain/Entities/Cliente.cs
+++ SolutionName/SolutionName.Domain/Entities/Cliente.cs
@@ -1,3 +1,6 @@
+using SolutionName.Domain.Shared;
+using static System.Net.Mime.MediaTypeNames;
+
 namespace SolutionName.Domain.Entities
 {
     public class Cliente
@@ -8,12 +11,28 @@ namespace SolutionName.Domain.Entities
             Nome = nome;
             Email = email;
             MultiplicadorBase = multiplicadorBase;
+
+            ValidateEntity();
         }
 
         public Guid Id { get; private set; }
         public string Nome { get; private set; }
         public string Email { get; private set; }
         public decimal MultiplicadorBase { get; private set; }
+
+        public void ValidateEntity()
+        {
+            AssertionConcern.AssertArgumentNotEmpty(Nome, "O nome não pode estar vazio!");
+            AssertionConcern.AssertArgumentNotEmpty(Email, "O email não pode estar vazio!");
+
+            AssertionConcern.AssertArgumentLength(Nome, 60, "O nome deve ter até 60 caracteres!");
+            AssertionConcern.AssertArgumentLength(Email, 100, "O email deve ter até 100 caracteres!");
+
+            AssertionConcern.AssertArgumentNotEqualZero(MultiplicadorBase, "O multiplicadorBase deve ser maior que 0");
+            AssertionConcern.AssertArgumentNotNegative(MultiplicadorBase, "O multiplicadorBase deve ser maior que 0");
+        }
+
+
     }
 
 }
